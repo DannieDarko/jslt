8c47ded4da1b60f5511058f544c3fac5c10703eb
Daniel Rexin <daniel@medinion.de>
2026-02-25 10:22:09 +0100

    Optimizes speed by utilizing builtin deepcopy. Updates formatting.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 setup(
   name="JSLT",
-  version="1.0.5",
+  version="1.1.0",
   description="JSON transformation tool",
   long_description=long_description,
   long_description_content_type="text/markdown",
