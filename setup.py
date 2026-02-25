dbcae87465aca40f4773fc62e1c8fa7ee8d2de34
Daniel Rexin <daniel@medinion.de>
2026-02-25 10:28:44 +0100

    Adds jsl:vars function to define multiple vars. Adds inheritance for jsl:each template of vars from current context

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / "README.md").read_text(encoding="utf-8")
 setup(
   name="JSLT",
-  version="1.1.0",
+  version="1.2.0",
   description="JSON transformation tool",
   long_description=long_description,
   long_description_content_type="text/markdown",
