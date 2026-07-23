7a9fcf54e2ad1e2d126810fda2b89451e8b8c24a
Daniel Rexin <daniel@medinion.de>
2026-07-23 09:13:20 +0200

    Adds example for custom functions.

---
+++ examples/custom_functions.py
@@ -0,0 +1,24 @@
+import logging
+from jslt.engine.functions import Functions
+from jslt.engine.transform import JSLT
+
+class CustomFunctions(Functions):
+  def _custom_decorate_string(ctx: JSLT.Context, value: str):
+    return f"### {value} ###"
+
+def run_tranform():
+  tmpl = {
+    'root': {
+      'my_value': 'some',
+      'custom_value': {
+        'custom:decorate_string': 'static value'
+      }
+    }
+  }
+  
+  jslt_engine = JSLT(tmpl)
+  print(jslt_engine.transform({}))
+  
+if __name__ == '__main__':
+  logging.basicConfig(level=logging.DEBUG)
+  run_tranform()
